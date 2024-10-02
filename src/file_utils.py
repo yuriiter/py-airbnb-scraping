ab51a348230fcb1a5097182f553a91feb74a6881
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-10-02 04:24:29 +0200

    make more unique name for auto-generated output file name

--- src/file_utils.py
+++ src/file_utils.py
@@ -1,11 +1,12 @@
 import time
 from datetime import datetime
 import os
+import hashlib
 
 def create_if_not_exists(path):
     """
     Create directories recursively if they don't exist.
-    
+
     If the path ends with '/', it treats the path as a directory.
     If the path does not end with '/', it treats the path as a file
     and ensures that the directory exists.
@@ -19,7 +20,11 @@ def create_if_not_exists(path):
             open(path, 'a').close()
 
 def default_output_file(query):
-    """Generate a default output filename based on the query and current date."""
+    """Generate a default output filename based on the query, current date, and a hash string."""
     now = datetime.now()
     num_of_milliseconds = int(time.time() * 1000) % 1000
-    return f"generated/{query}_{now.strftime('%Y%m%d')}_{num_of_milliseconds}.csv"
+
+    hash_object = hashlib.md5(query.encode())
+    hash_string = hash_object.hexdigest()[:6]
+
+    return (f"generated/{query}_{now.strftime('%Y%m%d_%H%M%S')}_{num_of_milliseconds}_{hash_string}.csv")
